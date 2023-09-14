21c058ab94310c63a3b8837694e06aa2cad66a81
Jonathan Simmons <jsim247@gmail.com>
2023-09-14 00:55:42 -0500

    upgraded versions and ran to confirm

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
