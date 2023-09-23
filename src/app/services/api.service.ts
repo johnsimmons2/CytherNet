30656274c8bca81133616530fc1bfae64523cc85
Jonathan Simmons <jsim247@gmail.com>
2023-09-23 18:10:20 -0500

    changed to correct address

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class ApiService {
 
-  readonly ROOT_URL = 'http://127.0.0.1:5000/';
+  readonly ROOT_URL = '';
 
   constructor(
       private router: Router,
